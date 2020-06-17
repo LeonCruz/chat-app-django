0064594dab3790698f67fc094d007a2ecec4a802
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-06-17 01:10:53 -0300

    Login do usuario

--- chat/views.py
+++ chat/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, Http404
 from chat.models import Message
 from django.utils import timezone
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['login']
+        user = authenticate(username=username)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/chat/')
+        else:
+            Http404()
+            
     return render(request, 'home.html')
 
 
 def add_message(request):
     message = request.POST['message']
 
-    if message is not '':
+    if message != '':
         Message.objects.create(body=message, time=timezone.now())
 
     return redirect('chat')
@@ -19,4 +30,4 @@ def add_message(request):
 
 def chat(request):
     messages = Message.objects.all()
-    return render(request, 'messages.html', {'messages': messages})
+    return render(request, 'chat.html', {'messages': messages})
--- chat_app/settings.py
+++ chat_app/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTHENTICATION_BACKENDS = [
+    'chat.auth.AuthWithoutPassword',
+    'django.contrib.auth.backends.ModelBackend',
+]
