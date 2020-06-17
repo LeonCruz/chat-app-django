d11e784fd561422a71b7d339fd7c4cceaeda6fed
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-06-17 01:42:46 -0300

    Validaçao do login do usuario

--- chat/auth.py
+++ chat/auth.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 class AuthWithoutPassword(ModelBackend):
     def authenticate(self, request, username=None):
-        User.objects.create(username=username)
         try:
             return User.objects.get(username=username)
         except User.DoesNotExist:
--- chat/views.py
+++ chat/views.py
@@ -2,16 +2,29 @@ from django.shortcuts import render, redirect, Http404
 from chat.models import Message
 from django.utils import timezone
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         username = request.POST['login']
-        user = authenticate(username=username)
 
-        if user is not None:
-            login(request, user)
+        if username == '':
+            error = 'O login não pode ser vazio'
+            return render(request, 'home.html', {'error': error})
+
+        try:
+            user = User.objects.create(username=username)
+        except IntegrityError:
+            error = 'O login já está em uso'
+            return render(request, 'home.html', {'error': error})
+       
+        user_auth = authenticate(username=username)
+
+        if user_auth is not None:
+            login(request, user_auth)
             return redirect('/chat/')
             
     return render(request, 'home.html')
