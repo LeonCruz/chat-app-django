1b47ecce7e68450e192f8a36d2b138c215b7e0cf
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-06-17 00:17:49 -0300

    Separaçao dos templates e das views

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,7 @@ from chat import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('add_message', views.add_message, name='add_message'),
+    path('chat/', views.chat, name='chat'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from chat.models import Message
 from django.utils import timezone
 
 
 # Create your views here.
 def home(request):
-    if request.method == 'POST':
-        message = request.POST['message']
+    return render(request, 'home.html')
 
-        if message is not '':
-            Message.objects.create(body=message, time=timezone.now())
 
+def add_message(request):
+    message = request.POST['message']
+
+    if message is not '':
+        Message.objects.create(body=message, time=timezone.now())
+
+    return redirect('chat')
+
+
+def chat(request):
     messages = Message.objects.all()
-    return render(request, 'home.html', {'messages': messages})
+    return render(request, 'messages.html', {'messages': messages})
