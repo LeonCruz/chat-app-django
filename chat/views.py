b621212c53001502fe847086fe84e9fa816f4a34
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-06-16 23:38:04 -0300

    Envio e exibição das mensagens

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from chat.models import Message
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        message = request.POST['message']
+
+        if message is not '':
+            Message.objects.create(body=message)
+
+    messages = Message.objects.all()
+    return render(request, 'home.html', {'messages': messages})
